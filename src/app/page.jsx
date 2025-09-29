a7280f81eea4631dd3f15f93d416f0bd864f0fb1
ahmdhsnzadeh <ahmdhasanzadeh@gmail.com>
2025-09-29 15:02:45 +0330

    new project of pralax3 add header

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useState } from "react";
+import Header from "./Nav/Header/Header";
+
+
+
+export default function Home() {
+  const [data , setData] = useState (()=>{
+      
+  })
+  return (
+    
+   <main className="bg-[var(--background)]">
+   <Header />
+   </main>
+  );
+}
